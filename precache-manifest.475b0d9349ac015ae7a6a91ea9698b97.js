c6347bf037c29a193d356674cc425426df7c2be2
glacticeye <alex.plocik.home@gmail.com>
2025-03-22 01:05:14 -0400

    Updates

---
+++ precache-manifest.475b0d9349ac015ae7a6a91ea9698b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e0862572e6dcead38f3fb3ebc79b4e",
+    "url": "/AI-GM-Adventure/index.html"
+  },
+  {
+    "revision": "155a10c3e389f58eb840",
+    "url": "/AI-GM-Adventure/static/css/main.e8f1bbce.chunk.css"
+  },
+  {
+    "revision": "43d4febbac71840e9792",
+    "url": "/AI-GM-Adventure/static/js/2.4aabcc69.chunk.js"
+  },
+  {
+    "revision": "d5a39fd591eaed0cfc76ff10340cbad9",
+    "url": "/AI-GM-Adventure/static/js/2.4aabcc69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155a10c3e389f58eb840",
+    "url": "/AI-GM-Adventure/static/js/main.610986a1.chunk.js"
+  },
+  {
+    "revision": "2c4a4d40b66949595100",
+    "url": "/AI-GM-Adventure/static/js/runtime-main.be355625.js"
+  }
+]);
